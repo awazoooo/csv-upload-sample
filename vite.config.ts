a772846d82767e291f990434635df147d511df6c
yozawa4662 <yuyaron01@gmail.com>
2025-03-02 23:28:03 +0900

    デプロイ用の GHA 追加

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  // GitHub Pagesにデプロイするためのベースパス設定
+  base: '/csv-upload-sample/',
 })
